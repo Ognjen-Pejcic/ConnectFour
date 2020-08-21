a1651c4333becc21559eac5c6684b41afbf3c194
kascez <zorankascelan60@gmail.com>
2020-08-21 11:07:44 +0200

    Database connection

--- ConnectFour/src/server/Main.java
+++ ConnectFour/src/server/Main.java
@@ -2,6 +2,9 @@ package server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import clienthandler.ClientHandler;
 import igra.Soba;
@@ -12,6 +15,8 @@ public class Main {
 	public static LinkedList<Soba> sobe = new LinkedList<>();
 
 	public static void main(String[] args) {
+		Main pro = new Main();
+		pro.createConnection();
 		int portNumber = 12000;
 		Socket socketZaKomunikaciju = null;
 		ServerSocket serverSocket = null;
@@ -29,7 +34,17 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println("Greska prilikom pokretanja servera");
 		}
-
+	}
+	
+	void createConnection () {
+		try {
+			Class.forName("com⁩.⁨mysql⁩.⁨jdbc⁩.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MYDB", "root", "root");
+			System.out.println("Uspjesno");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
