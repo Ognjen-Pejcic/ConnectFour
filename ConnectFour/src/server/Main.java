a3b08f67d561e5012e66dbf47f8da1cf74ab3380
User <User@User-PC.t-com.me>
2020-08-23 21:23:15 +0200

    Database reading and writing

--- ConnectFour/src/server/Main.java
+++ ConnectFour/src/server/Main.java
@@ -1,10 +1,13 @@
 package server;
 
+import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import clienthandler.ClientHandler;
 import igra.Soba;
@@ -13,6 +16,7 @@ public class Main {
 
 	public static LinkedList<ClientHandler> onlineKorisnici = new LinkedList<>();
 	public static LinkedList<Soba> sobe = new LinkedList<>();
+	Connection con;
 
 	public static void main(String[] args) {
 		Main pro = new Main();
@@ -38,9 +42,20 @@ public class Main {
 	
 	void createConnection () {
 		try {
-			Class.forName("com⁩.⁨mysql⁩.⁨jdbc⁩.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MYDB", "root", "root");
-			System.out.println("Uspjesno");
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+//			Statement stmt = con.createStatement();
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM USERS");
+//			while(rs.next()) {
+//				String name = rs.getString("name");
+//				System.out.println(name);
+//			}
+			System.out.println("Unesi igraca");
+			String nesto = "naky";
+			Statement stmt = con.createStatement();
+			String dpop = "INSERT INTO USERS VALUES('" + nesto + "')";
+			stmt.execute(dpop);
+			stmt.close(); //OBEVEZNO
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
